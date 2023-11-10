b94c0efd0a099122bb8a90370d8bb5970fe27877
... <2899531592@qq.com>
2023-11-10 15:59:53 +0800

    233

---
+++ d.c
@@ -0,0 +1,23 @@
+[33mcommit 379749ca55394b2cc190792308da743af8f88670[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: ... <2899531592@qq.com>
+Date:   Fri Nov 10 15:45:16 2023 +0800
+
+    sb
+
+[33mcommit 1ee9a1468d6b449a91662f22653b03bbc3408736[m
+Author: ... <2899531592@qq.com>
+Date:   Fri Nov 10 15:38:27 2023 +0800
+
+    rm min.c
+
+[33mcommit 401ed67b432dd3a6a6d019c1e7c66bc9b148114f[m
+Author: ... <2899531592@qq.com>
+Date:   Fri Nov 10 15:37:17 2023 +0800
+
+    rm test.c
+
+[33mcommit 95f9aa24ab3d6558c30a29791d7fb86bbb9f1a0d[m
+Author: ... <2899531592@qq.com>
+Date:   Thu Nov 9 19:59:55 2023 +0800
+
+    add new kk test.c
